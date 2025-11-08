b0591ea911bd3a0c97fd43b4fb50e36f9aefd68b
Max Van den Eynde <maxvdec@gmail.com>
2025-11-08 12:16:38 +0100

    Finished the first part of the landing page design

--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -8,4 +8,28 @@
 
 <div>
     <NavBar></NavBar>
+    <div class="mt-16 text-white">
+        <div
+            class="text-6xl font-sans font-bold flex justify-center items-center h-150 flex-row"
+        >
+            <div class="mt-16">
+                Build everything,<br />
+                and beyond.
+
+                <div
+                    class="mt-6 text-lg font-sans font-normal text-gray-400 w-96"
+                >
+                    Atlas is a powerful, flexible and easy-to-use Game Engine
+                    for developing amazing games, build worlds and interactive
+                    experiences, open to everyone.
+                </div>
+            </div>
+            <img
+                src="/images/landing.png"
+                alt="Landing Image"
+                class="h-110 w-110 -mt-10 mr-10"
+            />
+        </div>
+        <div class="h-150 items-center bg-amber-400"></div>
+    </div>
 </div>
