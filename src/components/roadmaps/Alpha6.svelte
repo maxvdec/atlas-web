8289d93819f1b13eae881506b317b8267c5da72b
Max Van den Eynde <maxvdec@gmail.com>
2025-12-24 13:59:16 +0100

    Added tracer announcements and updated the packages

--- src/components/roadmaps/Alpha6.svelte
+++ src/components/roadmaps/Alpha6.svelte
@@ -13,12 +13,12 @@
             {
                 title: "Create the Atlas Tracer app",
                 description: `Develop Atlas Tracer, a debug application for macOS that allows developers to test and debug rendering features of the Atlas Engine.`,
-                status: "planned",
+                status: "finished",
             },
             {
                 title: "Make Atlas interact with Atlas Tracer",
                 description: `Implement functionality in the Atlas Engine to interact seamlessly with the Atlas Tracer application, enabling efficient debugging and testing workflows.`,
-                status: "planned",
+                status: "finished",
             },
             {
                 title: "Integrate Jolt Physics into Atlas Engine",
